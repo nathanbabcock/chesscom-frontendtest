5ba2a6adbdaa4bcc5708aefc95323b674955b70b
nbabcock <nathan.r.babcock@gmail.com>
2023-05-22 14:49:24 -0500

    feat: create chessboard component

---
+++ src/components/ChessBoard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="board">
+    <div class="square" v-for="i in Array(63)" v-bind:key="i"/>
+  </div>
+</template>
+
+<style scoped>
+.board {
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(8, 1fr);
+  border: 3px solid white;
+  border-radius:  5px;
+  overflow: hidden;
+  width: 100%;
+  margin: 30px;
+}
+
+.square {
+  border: 1px solid white;
+
+  /* https://caniuse.com/mdn-css_properties_aspect-ratio */
+  /* ⚠ May not work in older browsers */
+  aspect-ratio: 1;
+}
+</style>
