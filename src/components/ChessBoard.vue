b270d8ce4a1859f7da8667a3a1daf26a7ad00b09
nbabcock <nathan.r.babcock@gmail.com>
2023-05-22 14:54:03 -0500

    feat: color grid square backgrounds

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="board">
-    <div class="square" v-for="i in Array(63)" v-bind:key="i"/>
+    <div class="square" v-for="i in Array(64)" v-bind:key="i" />
   </div>
 </template>
 
@@ -10,7 +10,7 @@
   grid-template-columns: repeat(8, 1fr);
   grid-template-rows: repeat(8, 1fr);
   border: 3px solid white;
-  border-radius:  5px;
+  border-radius: 5px;
   overflow: hidden;
   width: 100%;
   margin: 30px;
@@ -22,5 +22,17 @@
   /* https://caniuse.com/mdn-css_properties_aspect-ratio */
   /* ⚠ May not work in older browsers */
   aspect-ratio: 1;
+}
+
+/** Repeating pattern every 2 rows; alternate/diagonal chessboard grid background */
+.square:nth-child(16n + 1),
+.square:nth-child(16n + 3),
+.square:nth-child(16n + 5),
+.square:nth-child(16n + 7),
+.square:nth-child(16n + 10),
+.square:nth-child(16n + 12),
+.square:nth-child(16n + 14),
+.square:nth-child(16n + 16) {
+  background-color: rgba(255, 255, 255, 50%);
 }
 </style>
