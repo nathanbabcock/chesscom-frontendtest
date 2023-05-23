9c056d1df88016911e06a119ce92af4619d77f4b
nbabcock <nathan.r.babcock@gmail.com>
2023-05-23 13:33:00 -0500

    test: add unit tests for selecting squares

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  selectedSquares: number[]
+}>()
+</script>
+
+<script lang="ts">
+const RANKS = '12345678'.split('')
+const FILES = 'ABCDEFGH'.split('')
+
+function getLabel(n: number): string {
+  const rankIndex = RANKS.length - Math.floor(n / 8) - 1
+  const fileIndex = n % 8
+  const label = `${FILES[fileIndex]}${RANKS[rankIndex]}`
+  return label
+}
+</script>
+
 <template>
-  <aside class="sidebar" />
+  <aside class="sidebar">
+    <h2>Selected Squares</h2>
+    <ul class="squares-list">
+      <li class="square-item" v-for="(square, i) in selectedSquares" v-bind:key="i">
+        {{ getLabel(square) }}
+      </li>
+    </ul>
+  </aside>
 </template>
 
 <style scoped>
@@ -7,14 +32,40 @@
   background-color: rgba(255, 255, 255, 0.1);
   width: var(--sidebar-size);
   height: 100vh;
+  overflow: auto;
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1em;
+}
+
+.squares-list {
+  display: flex;
+  flex-direction: column;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  align-items: flex-start;
+  gap: 1em;
+}
+
+.square-item {
+  background: rgba(255, 255, 255, 10%);
+  padding: 1em;
+  border-radius: 0.25em;
+  margin: 0;
 }
 
 @media (orientation: portrait) {
   .sidebar {
     flex-shrink: 0;
     width: 100vw;
-    height: var(--sidebar-size)
+    height: var(--sidebar-size);
+  }
+
+  .squares-list {
+    flex-direction: row;
   }
 }
-
 </style>
--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,33 @@
-import { describe, it, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import { nextTick } from 'vue'
+import App from '../../App.vue'
 
-describe('HelloWorld', () => {
+describe('App', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(App)
+    expect(wrapper.text()).toContain('Selected Squares')
+  })
+
+  it('renders 64 squares', () => {
+    const wrapper = mount(App)
+    expect(wrapper.findAll('.square').length).toBe(64)
+  })
+
+  it('lists each selected square in sidebar', async () => {
+    const wrapper = mount(App)
+
+    // Click A8
+    const firstSquare = wrapper.find('.square')
+    firstSquare.trigger('click')
+    await nextTick()
+    expect(wrapper.text()).toContain('A8')
+
+    // Click B8
+    const secondSquare = wrapper.find('.square')
+    secondSquare.trigger('click')
+    await nextTick()
+    expect(wrapper.text()).toContain('A8')
   })
 })
