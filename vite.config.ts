42d3ec4f03579248b63fce8b46a64a7e201263f4
nbabcock <nathan.r.babcock@gmail.com>
2023-05-23 13:37:16 -0500

    chore: build release for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +11,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'docs'
   }
 })
